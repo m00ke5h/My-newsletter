77f2068acbc306f3f1e8d629fe28cc314e8a6353
m-ar-sh-h <pkdash1947.er@gmail.com>
2021-09-26 13:12:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const https = require('https');
+const url = 'https://us5.api.mailchimp.com/3.0/lists/8e58208b21';
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/signup.html")
+})
+
+app.post('/', (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name
+        }
+      }
+    ]
+  };
+
+  let jsonData = JSON.stringify(data);
+
+  let options = {
+    method: "POST",
+    auth: "kinetic:018ed3efc1b6808e1b2b6c4811b3cbe2-us5"
+  }
+
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
+    
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+})
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+})
+
+app.listen(port, () => console.log(`Server is running on port 3000!`))
+
+// API key - 018ed3efc1b6808e1b2b6c4811b3cbe2-us5
+// List ID - 8e58208b21
